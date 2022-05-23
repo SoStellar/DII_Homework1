8c2cdc25e2643920c1f401bb8b758ec8fb73b28b
SoStellar <githubpkaddress@gmail.com>
2022-05-23 22:08:13 +0700

    Homework 1 complete

---
+++ script.js
@@ -0,0 +1,71 @@
+var singleStudentResult = document.getElementById('single_student_result')
+var listStudentResult = document.getElementById('output')
+function addStudentData(student) {
+    let idElem = document.getElementById('id')
+    idElem.innerHTML = student.id
+    let studentIdElem = document.getElementById('studentId')
+    studentIdElem.innerHTML = student.studentId
+    let nameElem = document.getElementById('name')
+    nameElem.innerHTML = `${student.name} ${student.surname}`
+    let gpaElem = document.getElementById('gpa')
+    gpaElem.innerHTML = student.gpa
+    let profileElem = document.getElementById('image')
+    profileElem.setAttribute("src", student.image)
+}
+function addStudentToTable(index,student){
+    const tableBody = document.getElementById('tableBody')
+    let row = document.createElement('tr')
+    let cell = document.createElement('th')
+    cell.setAttribute('scope','row')
+    cell.innerHTML = index
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = `${student.name} ${student.surname}`
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cellbeforeImg= document.createElement('div')
+    cellbeforeImg.classList.add('img-fluid')
+    let img = document.createElement('img')
+    img.setAttribute('src',student.image)
+    img.height = 200
+    img.classList.add('img-thumbnails')
+    img.style.width = "5rem"
+    cellbeforeImg.appendChild(img)
+    cell.appendChild(cellbeforeImg)
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.description
+    row.appendChild(cell)
+    row.addEventListener('click',function(){
+        showStudentBlock(student)
+    })
+    row.appendChild(cell)
+    tableBody.appendChild(row)
+}
+function addStudentList(studentList){
+    let counter = 1
+    const tableBody = document.getElementById('tableBody')
+    tableBody.innerHTML = ''
+    for(student of studentList){
+        addStudentToTable(counter++,student)
+    }
+}
+function showAllStudents(){
+    fetch('https://dv-student-backend-2019.appspot.com/students')
+    .then((response)=>{
+        return response.json()
+    }).then(data =>{
+        addStudentList(data)
+    })
+}
+function showStudentBlock(student){
+    singleStudentResult.style.display = 'block'
+    addStudentData(student)
+}
+function showAllStudentBlock(student){
+    listStudentResult.style.display = 'block'
+    showAllStudents()
+}
+function onLoad(){
+    showAllStudentBlock()
+}
