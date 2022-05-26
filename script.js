5d1d8864e811d0dd3f125dc0a4b30fd76c22e416
SoStellar <githubpkaddress@gmail.com>
2022-05-26 19:34:03 +0700

    homework2 done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var singleStudentResult = document.getElementById('single_student_result')
-var listStudentResult = document.getElementById('output')
+var addUserDetail = document.getElementById('addUserDetail')
 function addStudentData(student) {
     let idElem = document.getElementById('id')
     idElem.innerHTML = student.id
@@ -11,62 +11,38 @@ function addStudentData(student) {
     gpaElem.innerHTML = student.gpa
     let profileElem = document.getElementById('image')
     profileElem.setAttribute("src", student.image)
-    profileElem.classList.add('img-thumbnails')
-    profileElem.style.width = "7rem"
 }
-function addStudentToTable(index,student){
-    const tableBody = document.getElementById('tableBody')
-    let row = document.createElement('tr')
-    let cell = document.createElement('th')
-    cell.setAttribute('scope','row')
-    cell.innerHTML = index
-    row.appendChild(cell)
-    cell = document.createElement('td')
-    cell.innerHTML = `${student.name} ${student.surname}`
-    row.appendChild(cell)
-    cell = document.createElement('td')
-    cellbeforeImg= document.createElement('div')
-    cellbeforeImg.classList.add('img-fluid')
-    let img = document.createElement('img')
-    img.setAttribute('src',student.image)
-    img.classList.add('img-thumbnails')
-    img.style.width = "7rem"
-    cellbeforeImg.appendChild(img)
-    cell.appendChild(cellbeforeImg)
-    row.appendChild(cell)
-    cell = document.createElement('td')
-    cell.innerHTML = student.description
-    row.appendChild(cell)
-    row.addEventListener('click',function(){
-        showStudentBlock(student)
-    })
-    row.appendChild(cell)
-    tableBody.appendChild(row)
-}
-function addStudentList(studentList){
-    let counter = 1
-    const tableBody = document.getElementById('tableBody')
-    tableBody.innerHTML = ''
-    for(student of studentList){
-        addStudentToTable(counter++,student)
-    }
-}
-function showAllStudents(){
-    fetch('https://dv-student-backend-2019.appspot.com/students')
-    .then((response)=>{
+function addStudentToDB(student){
+    fetch('https://dv-student-backend-2019.appspot.com/students',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(student)
+    }).then(response => {
+        if(response.status === 200){
         return response.json()
-    }).then(data =>{
-        addStudentList(data)
+    }else{
+        throw Error(response.statusText)
+    }
+    }).then(data => {
+        console.log('success',data)
+        showStudentBlock(data)
     })
+}
+function onAddStudentClick(){
+    let student = {}
+    student.name = document.getElementById('nameInput').value
+    student.surname = document.getElementById('surnameInput').value
+    student.studentId = document.getElementById('studentIdInput').value
+    student.gpa = document.getElementById('gpaInput').value
+    student.image = document.getElementById('imageLinkInput').value
+    addStudentToDB(student)
 }
 function showStudentBlock(student){
     singleStudentResult.style.display = 'block'
     addStudentData(student)
 }
-function showAllStudentBlock(student){
-    listStudentResult.style.display = 'block'
-    showAllStudents()
-}
 function onLoad(){
-    showAllStudentBlock()
+    addStudentToDB()
 }
